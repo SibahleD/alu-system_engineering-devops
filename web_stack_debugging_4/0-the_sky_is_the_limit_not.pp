d05d9176fe6752e2b0672a0e95267bffe6d64be1
SibahleD <s.dlamini2@alustudent.com>
2024-11-28 21:53:14 -0800

    hit

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,4 @@
 # Puppet script that increases the ulimit
-
-# Increase limit
 exec { 'increase-ulimit':
     command => '/bin/sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/\' /etc/default/nginx',
     path    => '/usr/local/bin/:/bin/'
