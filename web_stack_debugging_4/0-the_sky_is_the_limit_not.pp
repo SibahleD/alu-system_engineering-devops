35b94b603519c0e8f23af69305d8f0720c1e3e04
SibahleD <s.dlamini2@alustudent.com>
2024-11-28 21:33:11 -0800

    foe

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,12 @@
-# Puppet script that increases the nginx limit
+# Puppet script that increases the ulimit
 
 # Increase limit
 exec { 'increase-ulimit':
     command => '/bin/sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/\' /etc/default/nginx',
+    path    => '/usr/local/bin/:/bin/'
 }
 
 exec { 'restart nginx':
     command => '/usr/sbin/service nginx restart',
+    path    => '/etc/init.d/'
 }
